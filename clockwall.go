e9b5090a9ad27672ad6060a358102e2ef6d18e98
Cesar Lopez-Nataren <cesar@ciencias.unam.mx>
2016-03-05 13:13:10 -0800

    use channels to communicate between time readers and output clockwall

--- clockwall.go
+++ clockwall.go
@@ -9,11 +9,36 @@ import (
 	"sync"
 	"net"
 	"bufio"
+	"sort"
 )
 
 type Clock struct {
 	City string
 	HostnamePort string
+	Output chan CityTime
+}
+
+type CityTime struct {
+	City string
+	Time string
+}
+
+type ByCity []CityTime
+
+func (bc ByCity) Len() int {
+	return len(bc)
+}
+
+func (bc ByCity) Less(i, j int) bool {
+	return bc[i].City < bc[j].City
+}
+
+func (bc ByCity) Swap(i, j int) {
+	bc[i], bc[j] = bc[j], bc[i]
+}
+
+func (ct CityTime) String() string {
+	return fmt.Sprintf("%s: %v", ct.City, ct.Time)
 }
 
 func (c Clock) String() string {
@@ -27,23 +52,28 @@ func getClocks(args []string) []Clock {
 		clocks[i] = Clock {
 			City: values[0],
 			HostnamePort: values[1],
+			Output: make(chan CityTime),
 		}
 	}
 	return clocks
 }
 
-func copy(dst io.Writer, src io.Reader, city string) {
+func readtime(src io.Reader, c Clock) {
 	scanner := bufio.NewScanner(src)
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
 			continue
 		}
-		fmt.Fprintf(dst, fmt.Sprintf("\r%s: %v", city, scanner.Text()))
+		c.Output <- CityTime {
+			City: c.City,
+			Time: fmt.Sprintf("%s: %v", c.City, scanner.Text()),
+		}
 	}
 }
 
 func main() {
 	clocks := getClocks(os.Args[1:])
+	log.Println("created clocks")
 	var wg sync.WaitGroup
 	for _, c := range clocks {
 		wg.Add(1)
@@ -65,8 +95,33 @@ func main() {
 				log.Printf("error happened while trying to connect to '%s': %v", clock.String(), err)
 				return
 			}
-			copy(os.Stdout, conn, clock.City)
+			readtime(conn, clock)
 		}()
 	}
+	log.Println("setup time readers")
+	cityTimes := make([]chan CityTime, len(clocks))
+	for i, c := range clocks {
+		cityTimes[i] = c.Output
+	}
+	
+	// Output to std output
+	go func(cityCityTimes []chan CityTime) {
+		wg.Add(1)
+		defer wg.Done()
+		length := len(cityCityTimes)
+		vals := make([]CityTime, length)
+		messages := make([]string, length)
+		for {
+			for i, citytime := range cityCityTimes {
+				vals[i] = <-citytime
+			}
+			sort.Sort(ByCity(vals))
+			for i, citytime := range vals {
+				messages[i] = citytime.String()
+			}
+			fmt.Sprintf("\r%s", strings.Join(messages, "| "))
+		}
+	}(cityTimes)
+	log.Println("setup clockwall output")
 	wg.Wait()
 }
