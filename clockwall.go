5bbc1ef3a613aade4ee14b874eb410dda41b4227
Cesar Lopez-Nataren <cesar@ciencias.unam.mx>
2016-03-05 11:40:45 -0800

    wip clockwall

---
+++ clockwall.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"fmt"
+	"log"
+	"io"
+	"sync"
+	"net"
+)
+
+type Clock struct {
+	City string
+	HostnamePort string
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%s at %s", c.City, c.HostnamePort)
+}
+
+func getClocks(args []string) []Clock {
+	clocks := make([]Clock, len(args))
+	for i, v := range args {
+		values := strings.SplitN(v, "=", 2)
+		clocks[i] = Clock {
+			City: values[0],
+			HostnamePort: values[1],
+		}
+	}
+	return clocks
+}
+
+func copy(dst io.Writer, src io.Reader, city string) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatalf("error copying data to output stream: %v", err)
+	}
+}
+
+func main() {
+	clocks := getClocks(os.Args[1:])
+	var wg sync.WaitGroup
+	for _, c := range clocks {
+		wg.Add(1)
+		clock := c
+		go func() {
+			var conn net.Conn
+
+			// Make sure to clean up after ourselves
+			defer func() {
+				if conn != nil {
+					conn.Close()
+				}
+				wg.Done()
+			}()
+
+			// Connect and echo the put from the connection
+			conn, err := net.Dial("tcp", clock.HostnamePort)
+			if err != nil {
+				log.Printf("error happened while trying to connect to '%s': %v", clock.String(), err)
+				return
+			}
+			copy(os.Stdout, conn, clock.City)
+		}()
+	}
+	wg.Wait()
+}
