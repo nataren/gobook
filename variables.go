15e9827d101c540ef1e7d9316b33e183b6d3c9af
César López-Natarén <cesar@nataren.io>
2017-08-18 22:04:15 -0400

    semantics of Short Variable Declarations

--- variables.go
+++ variables.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type CustomTypeDefaultValues struct {
 
 	// Nil
 	f  func(n int) float64
 	ch chan int
-	// TODO: slice
+	sl []string
 
 	// 0
 	n int64
@@ -21,5 +24,15 @@ type CustomTypeDefaultValues struct {
 func main() {
 	var ct CustomTypeDefaultValues
 	fmt.Printf("%v\n", ct)
-	fmt.Printf("len(m)=%d\n", len(ct.m))
+	fmt.Printf("len(m)=%d, len(m.sl)=%d\n", len(ct.m), len(ct.sl))
+
+	f, err := os.Open("foo")
+
+	defer f.Close()
+
+	fmt.Println(f, err)
+	f2, err := os.Create("foo")
+	fmt.Println(f2, err)
+
+	defer f2.Close()
 }
